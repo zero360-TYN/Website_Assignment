f771bd377efd33d3b8068a387c4c655b7ba76cc3
ZERO <yuxuan674@gmail.com>
2026-03-08 21:55:02 +0800

    feat: Add main page, login page, and PHP base structure
    - Created index.php as the main entry point with header and footer includes.
    - Added login.html for user authentication with a search form and user menu.
    - Introduced php/_base.php to set the timezone for the application.
    - Developed php/_header.php and php/_footer.php for consistent page structure.
    - Implemented script.js for user menu toggle functionality and product management.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    $_title = 'Main';
+    require 'php/_base.php';
+    include 'php/_header.php';
+?>
+<main>
+    <p>&nbsp;&nbsp;this is main</p>
+    <script src="script.js"></script>
+</main>
+
+<?php 
+    include 'php/_footer.php';
+?>
