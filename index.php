18988979453fd106ead7c6cea2a21d5cb9211e3f
ZERO <yuxuan674@gmail.com>
2026-03-08 22:12:21 +0800

    fix: Update main content description for clarity

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     include 'php/_header.php';
 ?>
 <main>
-    <p>&nbsp;&nbsp;this is main</p>
+    <p>&nbsp;&nbsp;this is main,all the main content will write into main tag </p>
     <script src="script.js"></script>
 </main>
 
